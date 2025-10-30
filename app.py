b56df36104fafbca0fb871279ed9cf7ff0483614
Khalid Aloufi <kaloufi@mep.gov.sa>
2025-10-30 10:18:15 +0300

    added dev env

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
+import os
 
 @app.route('/')
 def hello_geek():
-    return '<div style=\"text-align: center;\"><h1>Hello from KodeKloud!</h1><p>Welcome to GCP DevOps Course.</p><p></p></div>'
+    pod_name = os.environ.get("HOSTNAME", "Unknown Pod")
+    return f'<div style="text-align: center;"><h1>Hello from GCP K8s!</h1><p>Welcome to GCP DevOps Course.</p><p>Pod Name: {pod_name}</p></div>'
+    #return '<div style=\"text-align: center;\"><h1>Hello from GCP K8s!</h1><p>Welcome to GCP DevOps Course.</p><p></p></div>'
 
 
 if __name__ == "__main__":
